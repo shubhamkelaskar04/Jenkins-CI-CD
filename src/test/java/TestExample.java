0264354867a232c1b424cccfea62795c7252f12b
Shubham Bhushan kelaskar <shubhamkelaskar25@gmail.com>
2026-03-15 11:57:50 +0530

    Added Test file

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestExample {
+
+    @Test
+    public void testAddition() {
+        int result = 5 + 3;
+        assertEquals(8, result);
+    }
+}
